1f6fd53ccf58df909c6cd7d7ee3e0d379d1b2bd3
Alex <pkms1@yandex.ru>
2020-02-13 16:00:41 +0300

    Change FirstRestController.java to FiresController.java. Delete Convert_main.java

---
+++ src/main/java/com/group/appName/FiresController.java
@@ -0,0 +1,26 @@
+
+package com.group.appName;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@RestController
+public class FiresController {
+
+    @RequestMapping(value = "/fires", method = RequestMethod.GET, produces = "application/json")
+    public String stub() throws IOException {
+        Convert convert = new Convert();
+        String filePath = "D://R2.xlsx";
+        String s = convert.start(filePath);
+        return s;
+    }
+}
+
+
+
+
+
