05559326623520ab7f18402f68f25fc3a9871088
Skurishin Vladislav <vladthelittleone@gmail.com>
2020-03-02 20:13:40 +0300

    change mapping + remove trash settings

--- src/main/java/com/group/appName/FileUploadController.java
+++ src/main/java/com/group/appName/FileUploadController.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
-	@RequestMapping(value = "/u", method = RequestMethod.POST)
+	@RequestMapping(value = "/api/upload", method = RequestMethod.POST)
 	public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile multipartFile) {
 		return "Uploaded: " + multipartFile.getSize() + " bytes";
 	}
