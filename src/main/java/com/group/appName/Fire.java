d3dc278fc657c339737728df01c58d4b4e3eadf2
AlexTheFireman <bizhov.alexander@gmail.com>
2020-10-28 08:42:21 +0300

    Add Lombok annotations and delete all getters and setters

--- src/main/java/com/group/appName/Fire.java
+++ src/main/java/com/group/appName/Fire.java
@@ -1,5 +1,10 @@
 package com.group.appName;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Fire {
     private String id;
     private String date;
@@ -40,172 +45,7 @@ public class Fire {
     public Fire() {
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getAddressObjectFireFeature() {
-        return addressObjectFireFeature;
-    }
-
-    public void setAddressObjectFireFeature(String addressObjectFireFeature) {
-        this.addressObjectFireFeature = addressObjectFireFeature;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getFireStation() {
-        return fireStation;
-    }
-
-    public void setFireStation(String fireStation) {
-        this.fireStation = fireStation;
-    }
-
-    public String getDestination() { return destination; }
-
-    public void setDestination(String destination) { this.destination = destination; }
-
-    public String getWhereWasTheFire() { return whereWasTheFire; }
-
-    public void setWhereWasTheFire(String whereWasTheFire) { this.whereWasTheFire = whereWasTheFire; }
-
-    public String getRescueWorks() { return rescueWorks; }
-
-    public void setRescueWorks(String rescueWorks) {this.rescueWorks = rescueWorks; }
-
-    public String getAmountOfRescuedPeople() { return amountOfRescuedPeople;}
-
-    public void setAmountOfRescuedPeople (String amountOfRescuedPeople) { this.amountOfRescuedPeople = amountOfRescuedPeople;}
-
-    public String getAmountOfEvacuatedPeople() { return amountOfEvacuatedPeople;}
-
-    public void setAmountOfEvacuatedPeople(String amountOfEvacuatedPeople) { this.amountOfEvacuatedPeople = amountOfEvacuatedPeople; }
-
-    public String getFireChiefRank() { return fireChiefRank; }
-
-    public void setFireChiefRank(String fireChiefRank) { this.fireChiefRank = fireChiefRank; }
-
-    public String getFireChiefName() { return fireChiefName; }
-
-    public void setFireChiefName(String fireChiefName) { this.fireChiefName = fireChiefName; }
-
-    public String getAmountOfSmokeGroups() { return amountOfSmokeGroups; }
-
-    public void setAmountOfSmokeGroups(String amountOfSmokeGroups) { this.amountOfSmokeGroups = amountOfSmokeGroups; }
-
-    public String getSmokeTime() {return smokeTime; }
-
-    public void setSmokeTime(String smokeTime) { this.smokeTime = smokeTime; }
-
-    public String getExtinguishingAgents() { return extinguishingAgents; }
-
-    public void setExtinguishingAgents(String extinguishingAgents) { this.extinguishingAgents = extinguishingAgents; }
-
-    public String getFirstEngine() { return firstEngine; }
-
-    public void setFirstEngine(String firstEngine) {this.firstEngine = firstEngine; }
-
-    public String getSecondEngine() { return secondEngine; }
-
-    public void setSecondEngine(String secondEngine) {this.secondEngine = secondEngine; }
-
-    public String getThirdEngine() { return thirdEngine; }
-
-    public void setThirdEngine(String thirdEngine) {this.thirdEngine = thirdEngine; }
-
-    public String getFirstReserve() { return firstReserve; }
-
-    public void setFirstReserve(String firstReserve) {this.firstReserve = firstReserve; }
-
-    public String getSecondReserve() { return secondReserve; }
-
-    public void setSecondReserve(String secondReserve) {this.secondReserve = secondReserve; }
-
-    public String getFirstSquadron() { return firstSquadron; }
-
-    public void setFirstSquadron(String firstSquadron) {this.firstSquadron = firstSquadron; }
-
-    public String getSecondSquadron() { return secondSquadron; }
-
-    public void setSecondSquadron(String secondSquadron) {this.secondSquadron = secondSquadron; }
-
-    public String getUsingHydrants() { return usingHydrants; }
-
-    public void setUsingHydrants(String usingHydrants) {this.usingHydrants = usingHydrants; }
-
-    public String getReportPDF() { return reportPDF; }
-
-    public void setReportPDF(String reportPDF) {this.reportPDF = reportPDF; }
-
-    public String getLocality() { return locality; }
-
-    public void setLocality(String locality) {this.locality = locality; }
-
-    public String getFireRank() { return fireRank; }
-
-    public void setFireRank(String fireRank) {this.fireRank = fireRank; }
-
-    public String getDetectionTime() { return detectionTime; }
-
-    public void setDetectionTime(String detectionTime) {this.detectionTime = detectionTime; }
-
-    public String getMessageTime() { return messageTime; }
-
-    public void setMessageTime(String messageTime) {this.messageTime = messageTime; }
-
-    public String getArrivalTime() { return arrivalTime; }
-
-    public void setArrivalTime(String arrivalTime) {this.arrivalTime = arrivalTime; }
-
-    public String getFirstNozzleTime() { return firstNozzleTime; }
-
-    public void setFirstNozzleTime(String firstNozzleTime) {this.firstNozzleTime = firstNozzleTime; }
-
-    public String getLocalizationTime() { return localizationTime; }
-
-    public void setLocalizationTime(String localizationTime) {this.localizationTime = localizationTime; }
-
-    public String getBurningLiquidationTime() { return burningLiquidationTime; }
-
-    public void setBurningLiquidationTime(String burningLiquidationTime) {this.burningLiquidationTime = burningLiquidationTime; }
-
-    public String getTotalLiquidationTime() { return totalLiquidationTime; }
-
-    public void setTotalLiquidationTime(String totalLiquidationTime) {this.totalLiquidationTime = totalLiquidationTime; }
-
-    public String getComment () { return comment; }
-
-    public void setComment(String comment) { this.comment = comment; }
-
-
-    @Override
+     @Override
     public String toString() {
         return "Fire [id=" + id + ", date=" + date + ", message=" + message + ", addressObjectFireFeature=" +
                 addressObjectFireFeature + ", district=" + district + ", fireStation=" + fireStation + ", destination=" +
