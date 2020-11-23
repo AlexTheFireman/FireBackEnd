d086d073f325f0ca4781b26e9e91ecc9fb02a7c0
Alex-fire <pkms1@yandex.ru>
2020-11-23 21:22:01 +0300

    Add @Transactional annotations to all methods

--- src/main/java/com/group/appName/service/FireService.java
+++ src/main/java/com/group/appName/service/FireService.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
@@ -14,15 +15,16 @@ import java.io.IOException;
 import java.util.List;
 
 @EnableAutoConfiguration
+@EnableTransactionManagement
 @Service
 public class FireService {
 
-@Autowired
-SessionFactory sessionFactory;
+    @Autowired
+    SessionFactory sessionFactory;
 
+    @Transactional
     public void addNewFile(File file) throws IOException, IllegalStateException, NullPointerException {
         Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
         FileEntity object = new FileEntity();
         String dataAsJSON = Convert.convertToJsonString(file.getPath());
         Byte[] arrayOfBytesWrapper = Convert.convertFromJsonToBytesArrayWrapper(dataAsJSON);
@@ -30,25 +32,19 @@ SessionFactory sessionFactory;
         object.setFileName(name);
         object.setFileData(arrayOfBytesWrapper);
         session.save(object);
-        session.getTransaction()
-                .commit();
     }
 
+    @Transactional
     public String getFile(String fileName) {
         FileEntity obj = null;
         Session session = sessionFactory.getCurrentSession();
         try {
-            session.beginTransaction();
             obj = session.get(FileEntity.class, fileName);
         } catch (Exception sqlException) {
             if (null != session.getTransaction()) {
                 System.out.println("\n.......Transaction Is Being Rolled Back.......");
                 session.getTransaction()
                         .rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
             }
         }
         if (obj != null) {
@@ -59,24 +55,20 @@ SessionFactory sessionFactory;
     @Transactional
     public void deleteFile (String fileName) {
         Session session = sessionFactory.getCurrentSession();
-
         System.out.println("File name = " + fileName);
         session.createQuery("DELETE FROM FileEntity WHERE fileName = :fileName")
                 .setParameter("fileName", fileName)
                 .executeUpdate();
     }
 
+    @Transactional
     public List getAll() {
         Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
         List list = session.createQuery("SELECT FE.fileName FROM FileEntity AS FE ")
                 .list();
-
-        if (session != null) {
-            session.close();
-        }
         return list;
     }
+
 }
 
 
